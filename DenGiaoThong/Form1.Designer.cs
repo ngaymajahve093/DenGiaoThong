72a926cceb42c5e72f10e7db5c5b110f08921d2c
t.cantranvancong <t.cantranvancong@outlook.com>
2024-05-23 17:36:34 +0700

    5_36 23_05

--- DenGiaoThong/Form1.Designer.cs
+++ DenGiaoThong/Form1.Designer.cs
@@ -30,12 +30,368 @@ namespace DenGiaoThong
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.cbxComList = new System.Windows.Forms.ComboBox();
+            this.btnKetNoi = new System.Windows.Forms.Button();
+            this.lbConnectionState = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_R1 = new System.Windows.Forms.Button();
+            this.btnOFF = new System.Windows.Forms.Button();
+            this.lbDataReceive2 = new System.Windows.Forms.Label();
+            this.lbDataReceive1 = new System.Windows.Forms.Label();
+            this.lbTextE = new System.Windows.Forms.Label();
+            this.lbTextD = new System.Windows.Forms.Label();
+            this.btn_Y1 = new System.Windows.Forms.Button();
+            this.btn_G1 = new System.Windows.Forms.Button();
+            this.btn_G2 = new System.Windows.Forms.Button();
+            this.btn_Y2 = new System.Windows.Forms.Button();
+            this.btn_R2 = new System.Windows.Forms.Button();
+            this.lbDataReceive0 = new System.Windows.Forms.Label();
+            this.btn_Sang = new System.Windows.Forms.Button();
+            this.btn_Toi = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_G2x = new System.Windows.Forms.Button();
+            this.btn_Y2x = new System.Windows.Forms.Button();
+            this.btn_R2x = new System.Windows.Forms.Button();
+            this.lbTextEx = new System.Windows.Forms.Label();
+            this.lbTextDx = new System.Windows.Forms.Label();
+            this.btn_G1x = new System.Windows.Forms.Button();
+            this.btn_Y1x = new System.Windows.Forms.Button();
+            this.btn_R1x = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbxComList
+            // 
+            this.cbxComList.FormattingEnabled = true;
+            this.cbxComList.Location = new System.Drawing.Point(22, 25);
+            this.cbxComList.Margin = new System.Windows.Forms.Padding(4);
+            this.cbxComList.Name = "cbxComList";
+            this.cbxComList.Size = new System.Drawing.Size(180, 26);
+            this.cbxComList.TabIndex = 0;
+            this.cbxComList.DropDown += new System.EventHandler(this.cbxComList_DropDown);
+            // 
+            // btnKetNoi
+            // 
+            this.btnKetNoi.AutoSize = true;
+            this.btnKetNoi.Enabled = false;
+            this.btnKetNoi.Location = new System.Drawing.Point(222, 21);
+            this.btnKetNoi.Name = "btnKetNoi";
+            this.btnKetNoi.Size = new System.Drawing.Size(77, 35);
+            this.btnKetNoi.TabIndex = 1;
+            this.btnKetNoi.Text = "Connect";
+            this.btnKetNoi.UseVisualStyleBackColor = true;
+            this.btnKetNoi.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lbConnectionState
+            // 
+            this.lbConnectionState.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbConnectionState.Location = new System.Drawing.Point(0, 858);
+            this.lbConnectionState.Name = "lbConnectionState";
+            this.lbConnectionState.Size = new System.Drawing.Size(889, 18);
+            this.lbConnectionState.TabIndex = 2;
+            this.lbConnectionState.Text = "Loading";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_R1
+            // 
+            this.btn_R1.Location = new System.Drawing.Point(236, 18);
+            this.btn_R1.Name = "btn_R1";
+            this.btn_R1.Size = new System.Drawing.Size(69, 65);
+            this.btn_R1.TabIndex = 3;
+            this.btn_R1.UseVisualStyleBackColor = true;
+            this.btn_R1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnOFF
+            // 
+            this.btnOFF.Location = new System.Drawing.Point(519, 18);
+            this.btnOFF.Name = "btnOFF";
+            this.btnOFF.Size = new System.Drawing.Size(95, 41);
+            this.btnOFF.TabIndex = 3;
+            this.btnOFF.Text = "OFF";
+            this.btnOFF.UseVisualStyleBackColor = true;
+            this.btnOFF.Click += new System.EventHandler(this.btnOFF_Click);
+            // 
+            // lbDataReceive2
+            // 
+            this.lbDataReceive2.AutoSize = true;
+            this.lbDataReceive2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbDataReceive2.Location = new System.Drawing.Point(0, 840);
+            this.lbDataReceive2.Name = "lbDataReceive2";
+            this.lbDataReceive2.Size = new System.Drawing.Size(43, 18);
+            this.lbDataReceive2.TabIndex = 4;
+            this.lbDataReceive2.Text = "Data:";
+            // 
+            // lbDataReceive1
+            // 
+            this.lbDataReceive1.AutoSize = true;
+            this.lbDataReceive1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbDataReceive1.Location = new System.Drawing.Point(0, 822);
+            this.lbDataReceive1.Name = "lbDataReceive1";
+            this.lbDataReceive1.Size = new System.Drawing.Size(43, 18);
+            this.lbDataReceive1.TabIndex = 5;
+            this.lbDataReceive1.Text = "Data:";
+            // 
+            // lbTextE
+            // 
+            this.lbTextE.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbTextE.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTextE.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTextE.ForeColor = System.Drawing.Color.Red;
+            this.lbTextE.Location = new System.Drawing.Point(556, 76);
+            this.lbTextE.Name = "lbTextE";
+            this.lbTextE.Size = new System.Drawing.Size(115, 72);
+            this.lbTextE.TabIndex = 6;
+            this.lbTextE.Text = "--";
+            this.lbTextE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbTextD
+            // 
+            this.lbTextD.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbTextD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTextD.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTextD.ForeColor = System.Drawing.Color.Red;
+            this.lbTextD.Location = new System.Drawing.Point(72, 158);
+            this.lbTextD.Name = "lbTextD";
+            this.lbTextD.Size = new System.Drawing.Size(115, 72);
+            this.lbTextD.TabIndex = 6;
+            this.lbTextD.Text = "--";
+            this.lbTextD.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Y1
+            // 
+            this.btn_Y1.Location = new System.Drawing.Point(236, 98);
+            this.btn_Y1.Name = "btn_Y1";
+            this.btn_Y1.Size = new System.Drawing.Size(69, 65);
+            this.btn_Y1.TabIndex = 3;
+            this.btn_Y1.UseVisualStyleBackColor = true;
+            this.btn_Y1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_G1
+            // 
+            this.btn_G1.Location = new System.Drawing.Point(236, 178);
+            this.btn_G1.Name = "btn_G1";
+            this.btn_G1.Size = new System.Drawing.Size(69, 65);
+            this.btn_G1.TabIndex = 3;
+            this.btn_G1.UseVisualStyleBackColor = true;
+            this.btn_G1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_G2
+            // 
+            this.btn_G2.Location = new System.Drawing.Point(722, 178);
+            this.btn_G2.Name = "btn_G2";
+            this.btn_G2.Size = new System.Drawing.Size(69, 65);
+            this.btn_G2.TabIndex = 7;
+            this.btn_G2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Y2
+            // 
+            this.btn_Y2.Location = new System.Drawing.Point(628, 178);
+            this.btn_Y2.Name = "btn_Y2";
+            this.btn_Y2.Size = new System.Drawing.Size(69, 65);
+            this.btn_Y2.TabIndex = 8;
+            this.btn_Y2.UseVisualStyleBackColor = true;
+            // 
+            // btn_R2
+            // 
+            this.btn_R2.Location = new System.Drawing.Point(534, 178);
+            this.btn_R2.Name = "btn_R2";
+            this.btn_R2.Size = new System.Drawing.Size(69, 65);
+            this.btn_R2.TabIndex = 9;
+            this.btn_R2.UseVisualStyleBackColor = true;
+            // 
+            // lbDataReceive0
+            // 
+            this.lbDataReceive0.AutoSize = true;
+            this.lbDataReceive0.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbDataReceive0.Location = new System.Drawing.Point(0, 804);
+            this.lbDataReceive0.Name = "lbDataReceive0";
+            this.lbDataReceive0.Size = new System.Drawing.Size(43, 18);
+            this.lbDataReceive0.TabIndex = 11;
+            this.lbDataReceive0.Text = "Data:";
+            // 
+            // btn_Sang
+            // 
+            this.btn_Sang.Location = new System.Drawing.Point(646, 18);
+            this.btn_Sang.Name = "btn_Sang";
+            this.btn_Sang.Size = new System.Drawing.Size(95, 41);
+            this.btn_Sang.TabIndex = 3;
+            this.btn_Sang.Text = "Sáng";
+            this.btn_Sang.UseVisualStyleBackColor = true;
+            this.btn_Sang.Click += new System.EventHandler(this.btn_Sang_Click);
+            // 
+            // btn_Toi
+            // 
+            this.btn_Toi.Location = new System.Drawing.Point(773, 18);
+            this.btn_Toi.Name = "btn_Toi";
+            this.btn_Toi.Size = new System.Drawing.Size(95, 41);
+            this.btn_Toi.TabIndex = 3;
+            this.btn_Toi.Text = "Tối";
+            this.btn_Toi.UseVisualStyleBackColor = true;
+            this.btn_Toi.Click += new System.EventHandler(this.btn_Toi_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.btn_G2x);
+            this.panel1.Controls.Add(this.btn_Y2x);
+            this.panel1.Controls.Add(this.btn_R2x);
+            this.panel1.Controls.Add(this.lbTextEx);
+            this.panel1.Controls.Add(this.lbTextDx);
+            this.panel1.Controls.Add(this.btn_G1x);
+            this.panel1.Controls.Add(this.btn_Y1x);
+            this.panel1.Controls.Add(this.btn_R1x);
+            this.panel1.Controls.Add(this.btn_G2);
+            this.panel1.Controls.Add(this.btn_Y2);
+            this.panel1.Controls.Add(this.btn_R2);
+            this.panel1.Controls.Add(this.lbTextD);
+            this.panel1.Controls.Add(this.lbTextE);
+            this.panel1.Controls.Add(this.btn_G1);
+            this.panel1.Controls.Add(this.btn_Y1);
+            this.panel1.Controls.Add(this.btn_R1);
+            this.panel1.Location = new System.Drawing.Point(23, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(850, 728);
+            this.panel1.TabIndex = 12;
+            // 
+            // btn_G2x
+            // 
+            this.btn_G2x.Location = new System.Drawing.Point(242, 468);
+            this.btn_G2x.Name = "btn_G2x";
+            this.btn_G2x.Size = new System.Drawing.Size(69, 65);
+            this.btn_G2x.TabIndex = 15;
+            this.btn_G2x.UseVisualStyleBackColor = true;
+            // 
+            // btn_Y2x
+            // 
+            this.btn_Y2x.Location = new System.Drawing.Point(148, 468);
+            this.btn_Y2x.Name = "btn_Y2x";
+            this.btn_Y2x.Size = new System.Drawing.Size(69, 65);
+            this.btn_Y2x.TabIndex = 16;
+            this.btn_Y2x.UseVisualStyleBackColor = true;
+            // 
+            // btn_R2x
+            // 
+            this.btn_R2x.Location = new System.Drawing.Point(54, 468);
+            this.btn_R2x.Name = "btn_R2x";
+            this.btn_R2x.Size = new System.Drawing.Size(69, 65);
+            this.btn_R2x.TabIndex = 17;
+            this.btn_R2x.UseVisualStyleBackColor = true;
+            // 
+            // lbTextEx
+            // 
+            this.lbTextEx.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbTextEx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTextEx.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTextEx.ForeColor = System.Drawing.Color.Red;
+            this.lbTextEx.Location = new System.Drawing.Point(196, 568);
+            this.lbTextEx.Name = "lbTextEx";
+            this.lbTextEx.Size = new System.Drawing.Size(115, 72);
+            this.lbTextEx.TabIndex = 14;
+            this.lbTextEx.Text = "--";
+            this.lbTextEx.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbTextDx
+            // 
+            this.lbTextDx.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbTextDx.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbTextDx.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTextDx.ForeColor = System.Drawing.Color.Red;
+            this.lbTextDx.Location = new System.Drawing.Point(636, 482);
+            this.lbTextDx.Name = "lbTextDx";
+            this.lbTextDx.Size = new System.Drawing.Size(115, 72);
+            this.lbTextDx.TabIndex = 13;
+            this.lbTextDx.Text = "--";
+            this.lbTextDx.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_G1x
+            // 
+            this.btn_G1x.Location = new System.Drawing.Point(540, 628);
+            this.btn_G1x.Name = "btn_G1x";
+            this.btn_G1x.Size = new System.Drawing.Size(69, 65);
+            this.btn_G1x.TabIndex = 10;
+            this.btn_G1x.UseVisualStyleBackColor = true;
+            // 
+            // btn_Y1x
+            // 
+            this.btn_Y1x.Location = new System.Drawing.Point(540, 548);
+            this.btn_Y1x.Name = "btn_Y1x";
+            this.btn_Y1x.Size = new System.Drawing.Size(69, 65);
+            this.btn_Y1x.TabIndex = 11;
+            this.btn_Y1x.UseVisualStyleBackColor = true;
+            // 
+            // btn_R1x
+            // 
+            this.btn_R1x.Location = new System.Drawing.Point(540, 468);
+            this.btn_R1x.Name = "btn_R1x";
+            this.btn_R1x.Size = new System.Drawing.Size(69, 65);
+            this.btn_R1x.TabIndex = 12;
+            this.btn_R1x.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(889, 876);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lbDataReceive0);
+            this.Controls.Add(this.lbDataReceive1);
+            this.Controls.Add(this.lbDataReceive2);
+            this.Controls.Add(this.btn_Toi);
+            this.Controls.Add(this.btn_Sang);
+            this.Controls.Add(this.btnOFF);
+            this.Controls.Add(this.lbConnectionState);
+            this.Controls.Add(this.btnKetNoi);
+            this.Controls.Add(this.cbxComList);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbxComList;
+        private System.Windows.Forms.Button btnKetNoi;
+        private System.Windows.Forms.Label lbConnectionState;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_R1;
+        private System.Windows.Forms.Button btnOFF;
+        private System.Windows.Forms.Label lbDataReceive2;
+        private System.Windows.Forms.Label lbDataReceive1;
+        private System.Windows.Forms.Label lbTextE;
+        private System.Windows.Forms.Label lbTextD;
+        private System.Windows.Forms.Button btn_Y1;
+        private System.Windows.Forms.Button btn_G1;
+        private System.Windows.Forms.Button btn_G2;
+        private System.Windows.Forms.Button btn_Y2;
+        private System.Windows.Forms.Button btn_R2;
+        private System.Windows.Forms.Label lbDataReceive0;
+        private System.Windows.Forms.Button btn_Sang;
+        private System.Windows.Forms.Button btn_Toi;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbTextDx;
+        private System.Windows.Forms.Button btn_G1x;
+        private System.Windows.Forms.Button btn_Y1x;
+        private System.Windows.Forms.Button btn_R1x;
+        private System.Windows.Forms.Button btn_G2x;
+        private System.Windows.Forms.Button btn_Y2x;
+        private System.Windows.Forms.Button btn_R2x;
+        private System.Windows.Forms.Label lbTextEx;
     }
 }
 
--- DenGiaoThong/Form1.cs
+++ DenGiaoThong/Form1.cs
@@ -7,14 +7,260 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using  System.IO.Ports;
 
 namespace DenGiaoThong
 {
     public partial class Form1 : Form
     {
+        string chuoi;
+        delegate void SetTextCallback(string text);
+
+        List<Button>  btnList1;
+        List<Button>  btnList1x;
+        List<Button>  btnList2;
+        List<Button>  btnList2x;
+        List<Color> colorList;
         public Form1()
         {
             InitializeComponent();
+
+            btnList1 = new List<Button>
+            {
+                btn_R1, btn_Y1, btn_G1
+            };
+            btnList1x = new List<Button>
+            {
+                btn_R1x, btn_Y1x, btn_G1x
+            };
+            btnList2 = new List<Button>
+            {
+                btn_R2, btn_Y2, btn_G2
+            };
+            btnList2x = new List<Button>
+            {
+                btn_R2x, btn_Y2x, btn_G2x
+            };
+            colorList = new List<Color>
+            {
+                Color.Red,Color.Yellow,Color.Green
+            };
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = cbxComList.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.Open();
+            }
+            else
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                chuoi = serialPort1.ReadLine().ToString().Trim();
+                SetText(chuoi);
+
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Bạn mất kết nối lúc dữ liệu đang được đọc\n" +
+                                  "Rất tiếc, dữ liệu của bạn bị mất 1 phần\n" +
+                                  "Muộn rồi, đây chỉ là thông báo :)\n" +
+                                  "HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA",
+                                  "Hey MAN!!!"
+                                  , MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        string datakey="T";
+        string keyE = "E";
+        string keyD = "D";
+        public void SetText(string text)
+        {
+            if (this.lbDataReceive2.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.BeginInvoke(d, new object[] { text });
+            }
+            else
+            {
+                Console.WriteLine(text);
+                if (text.Contains("T"))
+                {
+                    string textEdit = text;
+                    textEdit = text.Replace(datakey, "");
+                    textEdit = replaceforNumber(textEdit);
+                    lbDataReceive1.Text = "Data1: " + textEdit;
+                     if (text.Contains(keyD) && text.Contains(keyE) && text.IndexOf(keyE) - text.IndexOf(keyD) > 1)
+                    {
+                        int value1;
+                        int value2;
+
+                        int lengtKeyD = keyD.Length;
+                        int poskN = textEdit.IndexOf(keyD);
+                        int posmm = textEdit.IndexOf(keyE);
+
+                        int.TryParse(textEdit.Substring(0, textEdit.IndexOf(keyD)), out value1);
+                        int.TryParse(textEdit.Substring(poskN + lengtKeyD, posmm - poskN - lengtKeyD), out value2);
+                        //Console.WriteLine(value1.ToString());   //textD
+                        //Console.WriteLine(value2.ToString());
+                        lbDataReceive2.Text = "D: " + value1.ToString() + " - E: " + value2.ToString();
+                        lbTextD.Text = value1.ToString();
+                        lbTextE.Text = value2.ToString();
+                        lbTextDx.Text = value1.ToString();
+                        lbTextEx.Text = value2.ToString();
+                    }
+
+                }
+                else if (text.Contains("X"))
+                {
+                    string textEdit = text;
+                    lbDataReceive0.Text = "Xdata: " + text;
+                    Console.WriteLine(lbDataReceive0.Text);
+                    int valuex;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int.TryParse(textEdit.Substring(i + 1, 1), out valuex);
+                            if (valuex == 1)
+                            {
+                                btnList1[i].BackColor = colorList[i];
+                                btnList1x[i].BackColor = colorList[i];
+                            }
+                            else
+                            {
+                                btnList1[i].BackColor = Color.White;
+                                btnList1x[i].BackColor = Color.White;
+                            }
+                        }                 
+                }
+                else if (text.Contains("Y"))
+                {
+                    string textEdit = text;
+                    lbDataReceive0.Text = "Ydata: " + text;
+                    Console.WriteLine(lbDataReceive0.Text);
+                    int valuex;
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int.TryParse(textEdit.Substring(i + 1, 1), out valuex);
+                            if (valuex == 1)
+                            {
+                                btnList2[i].BackColor = colorList[i];
+                                btnList2x[i].BackColor = colorList[i];
+                            }
+                            else
+                            {
+                                btnList2[i].BackColor = Color.White;
+                                btnList2x[i].BackColor = Color.White;
+                            }
+                        }                    
+                }
+                else if (text.Contains("W"))
+                {
+                    if (text=="W0")
+                    {
+                        btnOFF.BackColor = Color.Green;
+                        btn_Sang.BackColor = Color.White;
+                        btn_Toi.BackColor = Color.White;
+                    }
+                    else if (text == "W1")
+                    {
+                        btnOFF.BackColor = Color.White;
+                        btn_Sang.BackColor = Color.Green;
+                        btn_Toi.BackColor = Color.White;
+                    }
+                    else if (text == "W2")
+                    {
+                        btnOFF.BackColor = Color.White;
+                        btn_Sang.BackColor = Color.White;
+                        btn_Toi.BackColor = Color.Green;
+                    }
+                }
+                else
+                {
+                    lbDataReceive1.Text = "Data2: " + text;
+                }
+
+            }
+        }
+        
+        public string replaceforNumber(string textreplace)      //Thay dấu chấm thành phẩy cho số thập phân
+        {
+            //return textreplace.Replace(txtRepalce1.Text, txtReplace2.Text);
+            return textreplace.Replace(",", ".");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                btnKetNoi.Text = "Ngắt kết nối";
+                lbConnectionState.Text = "Connected";
+                lbConnectionState.ForeColor = Color.Green;
+                cbxComList.Enabled = false;
+                //pnlControl.Enabled = true;
+            }
+            else
+            {
+                btnKetNoi.Text = "Kết nối";
+                lbConnectionState.Text = "Disonnected";
+                lbConnectionState.ForeColor = Color.Red;
+                cbxComList.Enabled = true;
+                //pnlControl.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("haha");
+        }
+
+
+        public void getPortNameFn()
+        {
+            Console.WriteLine("getPortName nhe");
+            if (SerialPort.GetPortNames().Length > 0)
+            {
+                cbxComList.DataSource = SerialPort.GetPortNames();
+                btnKetNoi.Enabled = true;
+            }
+            else
+            {
+                btnKetNoi.Enabled = false;
+            }
+
+        }
+
+        private void cbxComList_DropDown(object sender, EventArgs e)
+        {
+            getPortNameFn();
+        }
+
+        private void lbTest_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnOFF_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("M0");
+        }
+
+        private void btn_Sang_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("M1");
+        }
+
+        private void btn_Toi_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("M2");
+        }
+
     }
 }
